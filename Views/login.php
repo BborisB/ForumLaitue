a61dbc12fdbf4acd4a10b1e09eb645d53a71e5d7
Boris <b.boris.2001@gmail.com>
2023-11-22 22:02:05 +0100

    form action fix

--- Views/login.php
+++ Views/login.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php require_once "../Includes/head.php";
-    require_once "../Controllers/loginController.php";?>
+    require_once "../Controllers/loginController.php"; ?>
     <title>Se connecter</title>
     <link rel="stylesheet" href="../CSS/login.css">
 </head>
+
 <body>
     <div class="page">
         <h1>Se connecter</h1>
-        <form class="myForm myFormColumn" id="loginForm" action="login.php" method="post">
+        <form class="myForm myFormColumn" id="loginForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <span class="fieldError" id="loginErrorText"></span>
             <div class="label-input">
                 <label for="email">Email (*)</label>
-                <input type="text" name="email" id="email" value="<?php echo $email?>">
-                <span class="fieldError" id="emailErrorText"><?php echo $emailErrorText?></span>
+                <input type="text" name="email" id="email" value="<?php echo $email ?>">
+                <span class="fieldError" id="emailErrorText"><?php echo $emailErrorText ?></span>
             </div>
             <div class="label-input">
                 <label for="password">Mot de passe (*)</label>
-                <input type="password" name="password" id="password" value="<?php echo $password?>">
-                <span class="fieldError" id="passwordErrorText"><?php echo $passwordErrorText?></span>
+                <input type="password" name="password" id="password" value="<?php echo $password ?>">
+                <span class="fieldError" id="passwordErrorText"><?php echo $passwordErrorText ?></span>
             </div>
             <div class="multipleElement">
                 <input class="button" type="submit" value="Se connecter" id="submitBtn">
@@ -31,4 +33,5 @@
     <script src="../JavaScript/button.js"></script>
     <!-- <script src="../JavaScript/login.js"></script> -->
 </body>
+
 </html>
