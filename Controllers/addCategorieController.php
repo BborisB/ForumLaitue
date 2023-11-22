1af3e7caf33d1525a17b3c340e5948177dfedee5
Boris <b.boris.2001@gmail.com>
2023-11-22 19:32:24 +0100

    add categorie

--- Controllers/addCategorieController.php
+++ Controllers/addCategorieController.php
@@ -4,22 +4,32 @@ $connect = connect();
 $catTitle = $catTitleErrorText = $catDesc = $catDescTextError = "";
 if (isset($_POST["catTitle"]) && isset($_POST["catDesc"]))
 {
+    $canAdd = true;
     $catTitle = $_POST["catTitle"];
     $catDesc = $_POST["catDesc"];
     if($catTitle==="")
     {
         $catTitleErrorText = "Ce champ est requis.";
+        $canAdd = false;
+    }
+    else
+    {
+        $catTitleErrorText = "";
     }
     if($catDesc==="")
     {
         $catDescTextError = "Ce champ est requis.";
+        $canAdd = false;
     }
-    $req = $connect->prepare("INSERT INTO utilisateur (email, nom, prenom, motDePasse, photo, derniereConnection) VALUES(?,?,?,?,?,?)");
-    $req->execute(array($email, $lastName, $firstName, password_hash($password, PASSWORD_BCRYPT), $pfpFile, null));
-    header("location: ../Views/registerSuccess.php");
-}
-else
-{
-    return false;
+    else
+    {
+        $catDescTextError = "";
+    }
+    if($canAdd)
+    {
+        $req = $connect->prepare("INSERT INTO categorie (titre, description) VALUES(?,?)");
+        $req->execute(array($catTitle, $catDesc));
+        header("location: ../Views/forum.php");
+    }
 }
 ?>
